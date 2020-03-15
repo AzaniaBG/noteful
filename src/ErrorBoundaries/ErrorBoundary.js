bb882d1025485a59fc7fabc11070bda61afc52c3
AzaniaBG <azaniazbaker@gmail.com>
2020-03-15 08:49:43 -0700

    added Error Boundary that wraps around most components

---
+++ src/ErrorBoundaries/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//use class for stateful Components
+export default class ErrorBoundary extends React.Component {
+    //initialize state with an error set to FALSE
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false
+        }
+
+    }
+    //create error boundary that displays UI re error to user (may help user understand error)
+    getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    render() {
+    //conditionally render error message if state's hasError is TRUE
+        if(this.state.hasError) {
+            return (
+                <h2>Heck! We haz err.</h2>
+            )
+        }
+        return <div>{this.props.children}</div>
+    }
+}
