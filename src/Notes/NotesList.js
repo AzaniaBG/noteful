6e17ba23ee82876a54d641dbce6a44849cf71fe0
AzaniaBG <azaniazbaker@gmail.com>
2020-03-27 15:50:39 -0700

    added class name and changed element tags to ul

--- src/Notes/NotesList.js
+++ src/Notes/NotesList.js
@@ -4,15 +4,17 @@ import AppContext from '../Context/AppContext';
 import PropTypes from 'prop-types';
 
 class NotesList extends React.Component {
+    
 
     static contextType = AppContext;
     render() {     
+        console.log(Date);
         let noteName;
         const folderId = this.props.match.params.id;
     //console.log(`folderId from NotesList is ${folderId}`);
         if(folderId === undefined || folderId === null) {
             noteName = this.context.notes.map((note) => (
-                <div key={note.id}>
+                <li key={note.id}>
                 <NavLink to={`/note/${note.id}`}>
                     <h3 key={note.id}>{note.name}</h3>
                 </NavLink>
@@ -20,32 +22,37 @@ class NotesList extends React.Component {
                     <button id="deleteNoteButton" onClick={() => this.context.handleDeleteButton(note.id)}>
                         Delete Note
                     </button> 
-                </div>
+                </li>
             ))
         } else {
             const filteredNotes = this.context.notes.filter((note) => {
+                    const date = note.modified;
+                    console.log(`date is ${date}`)
+                    const formatted = new Date(date);
+                    const formattedDate = formatted.toDateString();
+                    console.log(`formattedDate is`, formattedDate)
                     return note.folderId === folderId;
                 });
             noteName = filteredNotes.map((note) => (
                 <div key={note.id}>
-                    <h3 key={note.id}>
+                    <li key={note.id}>
                         <NavLink to={`/note/${note.id}`}>
                             {note.name}
                         </NavLink>
-                    </h3>
+                    </li>
                     <p>Modified: {note.modified}</p>
                 </div>
                 ))
             }
         return (
-            <div className="Main">
+            <ul className="Notes">
                 {noteName}
                 <button>
                     <NavLink to={"/addNote"}>
                         Add Note
                     </NavLink>
                 </button>
-            </div>
+            </ul>
         )
     }
 }
