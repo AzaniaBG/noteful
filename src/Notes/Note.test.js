c0eaaa398899706907e23b2c2476626fb861f619
AzaniaBG <azaniazbaker@gmail.com>
2020-05-16 14:47:10 -0700

    added test for NotesList

--- src/Notes/Note.test.js
+++ src/Notes/Note.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Note from './Note';
+import NotesList from './NotesList';
+
 
 describe(`Note component`, () => {
     const props = {
@@ -10,11 +12,18 @@ describe(`Note component`, () => {
         modified: new Date(2020, 15, 5)
     }
     it('renders a Note by default', () => {
+    //the shallow function creates a WRAPPER instance of the Component we want to interact with
         const wrapper = shallow(<Note />);
         expect(toJson(wrapper)).toMatchSnapshot()
     });
     it('renders the Note given props', () => {
         const wrapper = shallow(<Note {...props} />)
         expect(toJson(wrapper)).toMatchSnapshot()
+    });
+});
+describe(`NotesList component`, () => {
+    it('renders an empty array by default', () => {
+        const wrapper = shallow(<NotesList />);
+        expect(wrapper).toMatchSnapshot();
     })
 })
